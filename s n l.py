5ad4e2257b9cffa99661810b9f1e6a3bfa8ec976
Kasireddy-Raghavendra <kasireddyragavendra@gmail.com>
2026-03-04 02:20:46 +0530

    prefinal

--- s n l.py
+++ s n l.py
@@ -4,17 +4,28 @@ import time;
 pygame.init()
 
 screen = pygame.display.set_mode((600, 800))
-clock = pygame.time.Clock()                                             #FPS control
 pygame.display.set_caption("S & L ")
+logo=pygame.image.load("D:\\Python\\S&L logo.png").convert_alpha()
+pygame.display.set_icon(logo)
 p1=pygame.image.load("D:\\Python\\p1.png").convert_alpha()                 #player 1 img
 p2=pygame.image.load("D:\\Python\\p2.png").convert_alpha()
 scaledp1 = pygame.transform.scale(p1, (60, 60))
 scaledp2 = pygame.transform.scale(p2, (60, 60))
-SAM=pygame.image.load("D:\\Python\\S&L logo.png").convert_alpha()                 #sam img
-scaledSAM = pygame.transform.scale(SAM, (40,40))
-
 B=pygame.image.load("D:\\Python\\snlboard.png").convert_alpha() 
-board = pygame.transform.scale(B, (500, 570))                                       #board img  
+board = pygame.transform.scale(B, (500, 567)) 
+bg=pygame.image.load("D:\\Python\\snlbackground.png").convert_alpha()                                      #board img  
+
+coordinates = {1:(55,564),2:(105,564),3:(155,564),4:(202,564),5:(255,564),6:(305,564),7:(355,564),8:(405,564),9:(455,564),10:(505,564),
+               11:(505,508),12:(455,508),13:(405,508),14:(355,508),15:(305,508),16:(255,508),17:(202,508),18:(155,508),19:(105,508),20:(55,508),
+                21:(55,450),22:(105,450),23:(155,450),24:(202,450),25:(255,450),26:(305,450),27:(355,450),28:(405,450),29:(455,450),30:(505,450),
+                31:(505,396),32:(455,396),33:(405,396),34:(355,396),35:(305,396),36:(255,396),37:(202,396),38:(155,396),39:(105,396),40:(55,396),
+                41:(55,336),42:(105,336),43:(155,336),44:(202,336),45:(255,336),46:(305,336),47:(355,336),48:(405,336),49:(455,336),50:(505,336),
+                51:(505,280),52:(455,280),53:(405,280),54:(355,280),55:(305,280),56:(255,280),57:(202,280),58:(155,280),59:(105,280),60:(55,280),
+                61:(55,220),62:(105,220),63:(155,220),64:(202,220),65:(255,220),66:(305,220),67:(355,220),68:(405,220),69:(455,220),70:(505,220),
+                71:(505,161),72:(455,161),73:(405,161),74:(355,161),75:(305,161),76:(255,161),77:(202,161),78:(155,161),79:(105,161),80:(55,161),
+                81:(55,106),82:(105,106),83:(155,106),84:(202,106),85:(255,106),86:(305,106),87:(355,106),88:(405,106),89:(455,106),90:(505,106),
+                91:(505,52),92:(455,52),93:(405,52),94:(355,52),95:(305,52),96:(255,52),97:(202,52),98:(155,52),99:(105,52),100:(55,52)
+    }
 
 dice_images = [None] 
 for i in range(1, 7):                                                   #load dice imgs
@@ -48,12 +59,16 @@ def ladders(position):
 
 #snake function
 def snakes(position):
-    snake={99:4, 91:49, 86:46, 89:43, 37:19, 13:7}
+    snake={99:4, 91:49, 86:46, 80:43, 37:19, 13:7}
     if position in snake:
         return snake[position]
     return position
     
-
+def delay_and_ignore_input(ms):
+    start = pygame.time.get_ticks()
+    while pygame.time.get_ticks() - start < ms:
+        pygame.event.get()  # Clear all events (ignore input)
+        pygame.time.delay(10)
 
 #game variables
 current_player=1   
@@ -74,62 +89,121 @@ while running:
                 roll()
                 if current_player == 1:
                     dice_1 = current_dice
-                    player_position_1 += current_dice
+                    for i in range(current_dice):
+                        player_position_1 += 1
+                        screen.blit(bg,(0,0))
+                        screen.blit(board,(50,50))
+                        if player_position_1 > 0 and player_position_1 <= 100:
+                            x1,y1 = coordinates[player_position_1]
+                            screen.blit(scaledp1, (x1-15,y1))
+                        if player_position_2 > 0 and player_position_2 <= 100:
+                            x2, y2 = coordinates[player_position_2]
+                            screen.blit(scaledp2, (x2-7, y2))
+
+                        screen.blit(text_surface,(200,725)) 
+                        screen.blit(position_1,(5,5)) 
+                        screen.blit(position_2,(350,5))
+                        
+                        if dice_1 > 0:
+                            pygame.draw.rect(screen, (240,236,89), (40, 665, 120, 120),border_bottom_right_radius=25,border_top_left_radius=25)
+                            screen.blit(dice_images[dice_1], (50, 675))
+                        if dice_2 > 0:
+                            
+                            screen.blit(dice_images[dice_2], (450, 675))
+                            
+
+                        pygame.display.flip()
+                        delay_and_ignore_input(200)
+
                     player_position_1 = ladders(player_position_1)               #ladder occurence
                     player_position_1 = snakes(player_position_1)                #snake occurence
                     position_1 = Arial.render(f"Player Position 1: {player_position_1}", True, (255, 93, 93))
+                    
                     if player_position_1 >= 100:
                         if player_position_1 > 100:
                             player_position_1 -= current_dice
                             position_1 = Arial.render(f"Player Position 1: {player_position_1}", True, (255, 93, 93))
                         if player_position_1 == 100:
                             position_1 = Arial.render("Congratulations! You win!", True, (255, 93, 93))
-                            time.sleep(2)
                             player_position_1 = 0
                             player_position_2 = 0
+                            delay_and_ignore_input(1000)
                     if current_dice == 6:
                         current_player = 1
                     else:
                         current_player = 2
+
                 else:
                     dice_2 = current_dice                 
-                    player_position_2 += current_dice
+                    for i in range(current_dice):
+                        player_position_2 += 1
+                        screen.blit(bg,(0,0))
+                        screen.blit(board,(50,50))
+                        if player_position_1 > 0 and player_position_1 <= 100:
+                            x1, y1 = coordinates[player_position_1]
+                            screen.blit(scaledp1, (x1-15,y1))
+                        if player_position_2 > 0 and player_position_2 <= 100:
+                            x2, y2 = coordinates[player_position_2]
+                            screen.blit(scaledp2, (x2-7, y2)) 
+
+                        screen.blit(text_surface,(200,725)) 
+                        screen.blit(position_1,(5,5)) 
+                        screen.blit(position_2,(350,5)) 
+                        
+                        if dice_1 > 0:
+                            
+                            screen.blit(dice_images[dice_1], (50, 675))
+                            
+                        if dice_2 > 0:
+                            pygame.draw.rect(screen, (240,236,89), (440, 665, 120, 120),border_bottom_right_radius=25,border_top_left_radius=25)
+                            screen.blit(dice_images[dice_2], (450, 675))
+
+                        pygame.display.flip()
+                        delay_and_ignore_input(200)
+
                     player_position_2 = ladders(player_position_2)              #ladder occurence
                     player_position_2 = snakes(player_position_2)               #sanke occurence
                     position_2 = Arial.render(f"Player Position 2: {player_position_2}", True, (255, 93, 93))
+                   
                     if player_position_2 >= 100:
                         if player_position_2 > 100:
                             player_position_2 -= current_dice
                             position_2 = Arial.render(f"Player Position 2: {player_position_2}", True, (255, 93, 93))
                         if player_position_2 == 100:
                             position_2 = Arial.render("Congratulations! You win!", True, (255, 93, 93))
-                            time
                             player_position_1 = 0
                             player_position_2 = 0
+                            delay_and_ignore_input(1000)
                     if current_dice == 6:
                         current_player = 2
                     else:
                         current_player = 1
-                print(player_position_1, player_position_2)             #debugging
-    screen.fill((194, 252, 255))                                        #background color
+    screen.blit(bg, (0, 0))                                       #background color
+    screen.blit(board,(50,50))                                        #draw board
 
+    if current_player == 1 and dice_1 > 0:
+        pygame.draw.rect(screen, (240,236,89), (40, 665, 120, 120), border_bottom_right_radius=25, border_top_left_radius=25)
     if dice_1 > 0:
-        screen.blit(dice_images[dice_1], (50, 675))                     #display dice image
+        screen.blit(dice_images[dice_1], (50, 675))
+    if current_player == 2 and dice_2 > 0:
+        pygame.draw.rect(screen, (240,236,89), (440, 665, 120, 120), border_bottom_right_radius=25, border_top_left_radius=25)
     if dice_2 > 0:
-        screen.blit(dice_images[dice_2], (355, 675))
-    screen.blit(board,(50,75))
+        screen.blit(dice_images[dice_2], (450, 675))
 
+    # Draw player 1 at their current position
+    if player_position_1 > 0 and player_position_1 <= 100:
+        x1, y1 = coordinates[player_position_1]
+        screen.blit(scaledp1, (x1-15,y1))
 
-    screen.blit(scaledSAM,(75,530))                                         #display sam img
+    # Draw player 2 at their current position
+    if player_position_2 > 0 and player_position_2 <= 100:
+        x2, y2 = coordinates[player_position_2]
+        screen.blit(scaledp2, (x2-7, y2))                              # Adjust position to center the player image
 
-
-    screen.blit(text_surface,(275,725))                                #refresh roll text
-    screen.blit(position_1,(5, 25))                                   #refresh position
-    screen.blit(position_2,(350, 25))
-    screen.blit(Arial.render(f"current player :{current_player}", True,(255, 86, 160)), (250, 750)) #current player turn
+    screen.blit(text_surface,(200,725))                                #refresh roll text
+    screen.blit(position_1,(5,5))                                   #refresh position
+    screen.blit(position_2,(350,5))
 
     pygame.display.flip()
-    clock.tick(30)   
+    pygame.time.Clock().tick(60)   
 pygame.quit()
-coordinates = {
-    1: (25,530),}
