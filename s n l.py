c77a953be633e3542a521f95b5fa45b7f2857de4
Kasireddy-Raghavendra <kasireddyragavendra@gmail.com>
2026-03-12 02:42:55 +0530

    99%

--- s n l.py
+++ s n l.py
@@ -1,10 +1,9 @@
 import pygame,random,sys,pygame.mixer_music
-
 pygame.init()
 pygame.mixer_music.load("D:\\Python\\snlbgm.mp3")
 pygame.mixer_music.play(-1)
-pygame.mixer_music.set_volume(0.3)
-ladderesound = pygame.mixer.Sound("D:\\Python\\laddereffect.mpeg")
+pygame.mixer_music.set_volume(0)
+laddersound = pygame.mixer.Sound("D:\\Python\\laddereffect.mpeg")
 snakesound = pygame.mixer.Sound("D:\\Python\\snakeeffect.MP3.mpeg")
 rollsound = pygame.mixer.Sound("D:\\Python\\rolleffect.MP3.mpeg")
 
@@ -16,18 +15,19 @@ logo=pygame.image.load("D:\\Python\\S&L logo.png").convert_alpha()
 pygame.display.set_icon(logo)
 scaledp1 = pygame.transform.scale(pygame.image.load("D:\\Python\\p1.png").convert_alpha(), (60, 60))
 scaledp2 = pygame.transform.scale(pygame.image.load("D:\\Python\\p2.png").convert_alpha(), (60, 60))
+congrats=pygame.transform.scale(pygame.image.load("D:\\Python\\congrats.png").convert_alpha(),(500,200))
 board = pygame.transform.scale(pygame.image.load("D:\\Python\\snlboard.png").convert_alpha(), (500, 567)) 
 woodbg = pygame.transform.scale(pygame.image.load("D:\\Python\\woodbg.png").convert_alpha(), (600, 800))
 bg=pygame.image.load("D:\\Python\\snlbackground.png").convert_alpha()  
 wood_img=pygame.transform.scale(pygame.image.load("D:\\Python\\button.png").convert_alpha(),(200,50))
-options_bg = pygame.transform.scale(pygame.image.load("D:\\Python\\options_bg.png").convert_alpha(),(600,800))
+restart_img=pygame.transform.scale(pygame.image.load("D:\\Python\\restart_img.png").convert_alpha(),(100,100))
 options_menu=pygame.transform.scale(pygame.image.load("D:\\Python\\options_menu.png").convert_alpha(),(400,350))
 SNL_text =pygame.transform.scale(pygame.image.load("D:\\Python\\SNLtext.png").convert_alpha(),(600,400)) 
 mute_img = pygame.transform.scale(pygame.image.load("D:\\Python\\0m.png").convert_alpha(), (100,100))  
 unmute_img = pygame.transform.scale(pygame.image.load("D:\\Python\\1m.png").convert_alpha(),(100,100))
 leave_img = pygame.transform.scale(pygame.image.load("D:\\Python\\leave.png").convert_alpha(),(75,75))  
 
-coordinates = {1:(55,564),2:(105,564),3:(155,564),4:(202,564),5:(255,564),6:(305,564),7:(355,564),8:(405,564),9:(455,564),10:(505,564),
+coordinates = {0:(275,704),1:(55,564),2:(105,564),3:(155,564),4:(202,564),5:(255,564),6:(305,564),7:(355,564),8:(405,564),9:(455,564),10:(505,564),
                11:(505,508),12:(455,508),13:(405,508),14:(355,508),15:(305,508),16:(255,508),17:(202,508),18:(155,508),19:(105,508),20:(55,508),
                 21:(55,450),22:(105,450),23:(155,450),24:(202,450),25:(255,450),26:(305,450),27:(355,450),28:(405,450),29:(455,450),30:(505,450),
                 31:(505,396),32:(455,396),33:(405,396),34:(355,396),35:(305,396),36:(255,396),37:(202,396),38:(155,396),39:(105,396),40:(55,396),
@@ -44,7 +44,7 @@ for i in range(1, 7):                                                   #load di
     scaled_img = pygame.transform.scale(img, (100,100))
     dice_images.append(scaled_img)
 
-text_surface = pygame.font.SysFont("Arial", 30).render("Press 'R' to roll", True, (185, 80, 255))   
+   
 position_1 = pygame.font.SysFont("Arial", 30).render("Player Position 1: ", True, (255, 93, 93)) 
 position_2 = pygame.font.SysFont("Arial", 30).render("Player Position 2: ", True, (255, 93, 93)) 
 
@@ -54,28 +54,32 @@ player_position_1=0
 player_position_2=0
 dice_1 = 0
 dice_2 = 0
+position_1 = pygame.font.SysFont("Arial", 30).render(f"Player Position 1: {player_position_1}", True, (255, 255, 255))
+position_2 = pygame.font.SysFont("Arial", 30).render(f"Player Position 2: {player_position_2}", True, (255, 255, 255))
 
 #roll function
 def roll():
     rollsound.play() 
-    global text_surface,current_dice
+    global current_dice
     dice= random.choices([1,2,3,4,5,6],[11,22,14,23,18,12])[0]                           #probability of dice
-    result_str = f"You rolled : {dice}"
+    
     current_dice = dice
-    text_surface = pygame.font.SysFont("Arial", 30).render(result_str, True, (185, 80, 255))
+    
            
-
 #ladder function
 def ladders(position):
     ladder={5:27, 9:51, 22:60, 28:54,44:79,53:69, 66:88, 71:92, 85:97}
     if position in ladder:
+        laddersound.play()
         return ladder[position]
     return position
 
 #snake function
 def snakes(position):
+    
     snake={99:4, 91:49, 86:46, 80:43, 37:19, 13:7}
     if position in snake:
+        snakesound.play()
         return snake[position]
     return position
     
@@ -115,16 +119,17 @@ def main_menu():
             if event.type == pygame.QUIT:
                 sys.exit()
               
-
 def play_game():
     running = True
+    
     while running:
         global current_player, player_position_1, player_position_2, dice_1, dice_2, position_1, position_2
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_q)or(event.type == pygame.MOUSEBUTTONDOWN and quit_roll_button.collidepoint(event.pos)):
-                main_menu()
+                restart()
             if (event.type == pygame.KEYDOWN and event.key == pygame.K_r)or(event.type == pygame.MOUSEBUTTONDOWN and roll_button.collidepoint(event.pos)):
                 roll()
                 if current_player == 1:
@@ -133,14 +138,14 @@ def play_game():
                         player_position_1 += 1
                         screen.blit(woodbg,(0,0))
                         screen.blit(board,(50,50))
-                        if player_position_1 > 0 and player_position_1 <= 100:
+                        if player_position_1 >= 0 and player_position_1 <= 100:
                             x1,y1 = coordinates[player_position_1]
                             screen.blit(scaledp1, (x1-15,y1))
-                        if player_position_2 > 0 and player_position_2 <= 100:
+                        if player_position_2 >= 0 and player_position_2 <= 100:
                             x2, y2 = coordinates[player_position_2]
                             screen.blit(scaledp2, (x2-7, y2))
 
-                        screen.blit(text_surface,(200,725)) 
+                        
                         screen.blit(position_1,(5,5)) 
                         screen.blit(position_2,(350,5))
                             
@@ -161,9 +166,9 @@ def play_game():
                             player_position_1 -= current_dice
                             position_1 = pygame.font.SysFont("Arial", 30).render(f"Player Position 1: {player_position_1}", True, (255,255,255))
                         if player_position_1 == 100:
-                            position_1 = pygame.font.SysFont("Arial", 30).render("Congratulations! You win!", True, (255,255,255))
+                            screen.blit(congrats)
                             player_position_1 = 0
-                            player_position_2 = 0
+                            player_position_2=0
                             delay_and_ignore_input(1000)
                     if current_dice == 6:
                         current_player = 1
@@ -176,13 +181,13 @@ def play_game():
                         player_position_2 += 1
                         screen.blit(woodbg,(0,0))
                         screen.blit(board,(50,50))
-                        if player_position_1 > 0 and player_position_1 <= 100:
+                        if player_position_1 >= 0 and player_position_1 <= 100:
                             x1, y1 = coordinates[player_position_1]
                             screen.blit(scaledp1, (x1-15,y1))
-                        if player_position_2 > 0 and player_position_2 <= 100:
+                        if player_position_2 >= 0 and player_position_2 <= 100:
                             x2, y2 = coordinates[player_position_2]
                         screen.blit(scaledp2, (x2-7, y2)) 
-                        screen.blit(text_surface,(200,725)) 
+                        
                         screen.blit(position_1,(5,5)) 
                         screen.blit(position_2,(350,5)) 
                             
@@ -206,7 +211,8 @@ def play_game():
                                 player_position_2 -= current_dice
                                 position_2 = pygame.font.SysFont("Arial", 30).render(f"Player Position 2: {player_position_2}", True, (255,255,255))
                         if player_position_2 == 100:
-                                position_2 = pygame.font.SysFont("Arial", 30).render("Congratulations! You win!", True, (255,255,255))
+                                
+                                screen.blit(congrats)
                                 player_position_1 = 0
                                 player_position_2 = 0
                                 delay_and_ignore_input(1000)
@@ -215,7 +221,9 @@ def play_game():
                     else:
                         current_player = 1
         screen.blit(woodbg, (0, 0))                                            #background color
-        screen.blit(board,(50,50))                                         #draw board
+        screen.blit(board,(50,50))
+        screen.blit(position_1,(5,5))
+        screen.blit(position_2,(350,5))                                         #draw board
 
         if current_player == 1 and dice_1 > 0:
             pygame.draw.rect(screen, (240,236,89), (40, 665, 120, 120), border_bottom_right_radius=25, border_top_left_radius=25)
@@ -227,17 +235,17 @@ def play_game():
             screen.blit(dice_images[dice_2], (450, 675))
 
         # Draw player 1 at their current position
-        if player_position_1 > 0 and player_position_1 <= 100:
+        if player_position_1 >= 0 and player_position_1 <= 100:
             x1, y1 = coordinates[player_position_1]
             screen.blit(scaledp1, (x1-15,y1))
 
         # Draw player 2 at their current position
-        if player_position_2 > 0 and player_position_2 <= 100:
+        if player_position_2 >= 0 and player_position_2 <= 100:
             x2, y2 = coordinates[player_position_2]
             screen.blit(scaledp2, (x2-7, y2))                              # Adjust position to center the player image
 
         screen.blit(leave_img,(-10,50))                                     #quit button
-        screen.blit(text_surface,(200,725))                                #refresh roll text
+                       
         screen.blit(position_1,(5,5))                                      #refresh position
         screen.blit(position_2,(350,5))
         screen.blit(pygame.transform.scale(wood_img,(120,50)),(240,670))
@@ -275,7 +283,44 @@ def options():
             if (event.type == pygame.MOUSEBUTTONDOWN and quit_rect.collidepoint(event.pos)) or (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
                 pygame.quit()
                 exit() 
-                           
+
+def restart():
+    while True:
+        global player_position_1, player_position_2,current_player,dice_1,dice_2,position_1,position_2
+        text=pygame.font.SysFont("Arial",50).render("Do you want to exit ?", True, (0,0,0))
+        restart_rect = pygame.Rect(250,400,100,100)
+        yes_rect = pygame.Rect(150, 350, 100, 50)
+        no_rect = pygame.Rect(350, 350, 100, 50)    
+        yes_text = pygame.font.SysFont("Arial", 30).render("Yes (Y)", True, (255, 255, 255))
+        no_text = pygame.font.SysFont("Arial", 30).render("No (N)", True, (255, 255, 255))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_y)or(event.type == pygame.MOUSEBUTTONDOWN and yes_rect.collidepoint(event.pos)):
+                main_menu()
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_n)or(event.type == pygame.MOUSEBUTTONDOWN and no_rect.collidepoint(event.pos)):
+                play_game()
+            if (event.type == pygame.MOUSEBUTTONDOWN and restart_rect.collidepoint(event.pos)):
+                player_position_1=0
+                player_position_2=0
+                dice_1=0
+                dice_2=0
+                current_player=1
+                screen.blit(position_1,(5,5))
+                screen.blit(position_2,(300,5))
+                return
+        screen.blit(options_menu,(100,200))
+        screen.blit(pygame.transform.scale(wood_img,(100,50)),(160,350))
+        screen.blit(pygame.transform.scale(wood_img,(100,50)),(360,350))
+        screen.blit(restart_img,(250,403))
+        screen.blit(yes_text, (yes_rect.x+20, yes_rect.y+5))
+        screen.blit(no_text, (no_rect.x+20, no_rect.y+5))
+        screen.blit(text,(120,250))
+        
+        pygame.display.flip()
 while True:
-  main_menu()
-  sys.exit()
+    main_menu()
+    sys.exit()
+
